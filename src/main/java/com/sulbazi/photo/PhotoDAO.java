771f4f8b6271bc0405201c31e6f0f255689991b8
jang,hee-jae <jangniel@gmail.com>
2024-10-24 11:35:07 +0900

    사진 올리기

--- src/main/java/com/sulbazi/photo/PhotoDAO.java
+++ src/main/java/com/sulbazi/photo/PhotoDAO.java
@@ -4,5 +4,9 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PhotoDAO {
+	/*
+	 * void fileSave(int store_idx, String newfile, int cti);
+	 */
+	void fileSave(PhotoDTO photoDTO);
 
 }
